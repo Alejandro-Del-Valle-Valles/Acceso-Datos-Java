000ffc6bdaeb86b2f8368f3897c55d06a69bb4df
Alejandro del Valle Vallés <alejandrodelvalle06@outlook.es>
2025-11-17 17:11:02 +0100

    Implemento un par de características de prueba al proyecto de la API

--- mi-primer-api/src/main/java/delvalle/valles/alejandro/mi_primer_api/MiPrimerApiApplication.java
+++ mi-primer-api/src/main/java/delvalle/valles/alejandro/mi_primer_api/MiPrimerApiApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class MiPrimerApiApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(MiPrimerApiApplication.class, args);
+        SpringApplication.run(MiPrimerApiApplication.class, args);
 	}
 
 }
--- mi-primer-api/src/main/java/delvalle/valles/alejandro/mi_primer_api/controllers/HelloControler.java
+++ mi-primer-api/src/main/java/delvalle/valles/alejandro/mi_primer_api/controllers/HelloControler.java
@@ -1,4 +1,22 @@
 package delvalle.valles.alejandro.mi_primer_api.controllers;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/hello")
 public class HelloControler {
+
+    @GetMapping("/world")
+    public String helloWorld(){
+        return "Hello World!";
+    }
+
+    @GetMapping("/{name}")
+    public String helloName(@PathVariable String name) {
+        return "Hola " + name + "!";
+    }
+
 }
