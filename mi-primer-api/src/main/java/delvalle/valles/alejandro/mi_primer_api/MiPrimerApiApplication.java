82e4e78a92de6f5962265e0e5eee7775fdfbc5a9
Alejandro Del Valle <alejandrodelvalle06@outlook.es>
2025-11-17 10:56:23 +0100

    Empiezo el 2º trimestre de Acceso a Datos

---
+++ mi-primer-api/src/main/java/delvalle/valles/alejandro/mi_primer_api/MiPrimerApiApplication.java
@@ -0,0 +1,13 @@
+package delvalle.valles.alejandro.mi_primer_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiPrimerApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiPrimerApiApplication.class, args);
+	}
+
+}
---
+++ mi-primer-api/src/main/java/delvalle/valles/alejandro/mi_primer_api/controllers/HelloControler.java
@@ -0,0 +1,4 @@
+package delvalle.valles.alejandro.mi_primer_api.controllers;
+
+public class HelloControler {
+}
